f98f8f5a9a78d77a9a42eab954c86213c79c0fef
rromul <rromul@gmail.com>
2020-11-15 15:02:37 +0300

    Double elements

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,15 @@
     <h1>
       Live placeholder test
     </h1>
-    <label for="sum" class="ctl-label color1">Some text:</label>
+    <label for="sum" class="ctl-label color1">Phone number:</label>
     <live-placeholder pattern="+9 (999) 999-99-99" fix-default show-placeholder>
-      <input id="sum" type="text" value="+7 ">
+      <input id="sum" type="text" v-model="phone" default-value="+7 ">
+    </live-placeholder>
+    <div class="hint-msg color1">от 50 000 до 1 000 000 ₽</div>
+
+    <label for="sum2" class="ctl-label color1">Phone number:</label>
+    <live-placeholder pattern="+9 (999) 999-99-99" fix-default show-placeholder mask-digit="_">
+      <input id="sum2" type="text" v-model="phone" default-value="+7 ">
     </live-placeholder>
     <div class="hint-msg color1">от 50 000 до 1 000 000 ₽</div>
 
@@ -23,6 +29,11 @@ export default {
   components: { LivePlaceholder },
   props: {
     msg: String
+  },
+  data () {
+    return {
+      phone: "+7 "
+    }
   }
 }
 </script>
